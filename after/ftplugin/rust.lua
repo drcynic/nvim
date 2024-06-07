0df3ded8fb3748f87d83c3a2de31c84feb9b3e45
cynic <daniel@szymanski.cc>
2024-06-07 20:16:52 +0200

    Add lsp keybinding for rust

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -9,3 +9,26 @@ vim.keymap.set( "n", "<leader>ca", function() vim.cmd.RustLsp('codeAction') end,
 vim.keymap.set( "n", "<leader>r", function() vim.cmd.RustLsp { 'runnables', bang = true } end, opts)
 vim.keymap.set( "n", "<leader>oc", function() vim.cmd.RustLsp('openCargo') end, opts)
 
+vim.keymap.set( "n", "<A-S-j>", function() vim.cmd.RustLsp { 'moveItem', 'down' } end, opts)
+vim.keymap.set( "n", "<A-S-k>", function() vim.cmd.RustLsp { 'moveItem', 'up' } end, opts)
+
+vim.keymap.set( "n", "<S-j>", function() vim.cmd.RustLsp('joinLines') end, opts)
+
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, opts)
+vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+vim.keymap.set("n", "<space>f", function()
+    vim.lsp.buf.format({ async = true })
+end, opts)
+
